5d37c0443186d1259de0cbafcc92a26ae9ca658d
Abel <rfong@ncsu.edu>
2019-12-03 23:43:23 -0500

    .gitignore is now working

---
+++ prime.py
@@ -0,0 +1,38 @@
+
+
+import math
+
+def get_primes(input_list):
+    result_list = list()
+
+    for element in input_list:
+    
+        if is_prime(element):
+            result_list.append(element)
+
+    return result_list
+
+# # or better yet...
+
+# def get_primes(input_list):
+#     return (element for element in input_list if is_prime(element))
+
+# not germane to the example, but here's a possible implementation of
+# is_prime...
+
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0: 
+                return False
+        return True
+    return False
+
+
+
+print(get_primes(list(range(1,100))))
+
